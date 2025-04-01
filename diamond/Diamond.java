c1c37710c75d4ffd3d7471fa038f1f1c708f0494
pkuselias <pkuselias@hartford.edu>
2025-03-31 22:23:20 -0400

    Remove unnecessary code for user input of 1

--- diamond/Diamond.java
+++ diamond/Diamond.java
@@ -30,58 +30,49 @@ public class Diamond {
 
     System.out.println();
 
-    if (dimensions != 1) {
-      if ((dimensions % 2) != 0) {
-        String space = " ", star = "*", finalSpaces, finalStars;
-        int spacesNumber = (dimensions - 1) / 2;
-        int starNumber = 1;
-        int lines = dimensions;
+    if ((dimensions % 2) != 0) {
+      String space = " ", star = "*", finalSpaces, finalStars;
+      int spacesNumber = (dimensions - 1) / 2;
+      int starNumber = 1;
+      int lines = dimensions;
 
-        for (int i = 1; i <= dimensions; i++) {
-          //if (spacesNumber < 0) {
-            //spacesNumber = -1 * spacesNumber;
-          //}
+      for (int i = 1; i <= dimensions; i++) {
+        finalSpaces = "";
 
-          finalSpaces = "";
-
-          if (spacesNumber >= 0) {
-            for (int j = 1; j <= spacesNumber; j++) {
-              finalSpaces += space;
-            }
-
-          } else {
-            for (int j = 1; j <= (-1 * spacesNumber); j++) {
-              finalSpaces += space;
-            }
+        if (spacesNumber >= 0) {
+          for (int j = 1; j <= spacesNumber; j++) {
+            finalSpaces += space;
           }
 
-          finalStars = "";
-
-          for (int k = 1; k <= starNumber; k++) {
-            finalStars += star;
-          }
-
-          System.out.println(finalSpaces + finalStars);
-
-          spacesNumber--;
-
-          if (starNumber < lines) {
-            starNumber += 2;
-
-          } else {
-            starNumber -= 2;
-            lines -= 2;
+        } else {
+          for (int j = 1; j <= (-1 * spacesNumber); j++) {
+            finalSpaces += space;
           }
         }
 
-      } else {
-        for (int i = 0; i < dimensions; i++) {
+        finalStars = "";
 
+        for (int k = 1; k <= starNumber; k++) {
+          finalStars += star;
+        }
+
+        System.out.println(finalSpaces + finalStars);
+
+        spacesNumber--;
+
+        if (starNumber < lines) {
+          starNumber += 2;
+
+        } else {
+          starNumber -= 2;
+          lines -= 2;
         }
       }
 
     } else {
-      System.out.println("*");
+      for (int i = 0; i < dimensions; i++) {
+
+      }
     }
 
     System.out.println();
