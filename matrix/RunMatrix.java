2f5ae2a89785bd3b05caecccd48d1ec0f69ce08f
pkuselias <pkuselias@hartford.edu>
2025-04-07 13:25:13 -0400

    Validate user input for size

--- matrix/RunMatrix.java
+++ matrix/RunMatrix.java
@@ -2,5 +2,34 @@ import java.util.Scanner;
 
 public class RunMatrix {
   public static void main(String[] args) {
+    int size;
+    boolean isValid = false;
+
+    Scanner scan = new Scanner(System.in);
+
+    System.out.printf("\nEnter a positive integer: ");
+
+    while (isValid == false) {
+      if (scan.hasNextInt()) {
+        size = scan.nextInt();
+        isValid = true;
+
+        if (size < 1) {
+          isValid = false;
+          System.out.printf("\nInvalid input. Please enter a positive integer: ");
+          scan.nextLine();
+        }
+
+      } else {
+        System.out.printf("\nInvalid input. Please enter a positive integer: ");
+        scan.nextLine();
+      }
+    }
+
+    scan.close();
+
+    System.out.println();
+
+
   }
 }
