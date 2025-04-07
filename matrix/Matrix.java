9843429fdbd5a85285a2de0643de3b9e56f51ff2
pkuselias <pkuselias@hartford.edu>
2025-04-07 14:51:50 -0400

    Update printMatrix method to color intended diagonal entries

--- matrix/Matrix.java
+++ matrix/Matrix.java
@@ -8,7 +8,11 @@ public class Matrix {
   public void printMatrix() {
     for (int i = 0; i < matrix.length; i++) {
       for (int j = 0; j < matrix[i].length; j++) {
-        System.out.printf("\t%d", matrix[i][j]);
+        if ((i + j) == (matrix.length - 1)) {
+          System.out.printf("\t\u001B[33m%d\u001B[0m", matrix[i][j]);
+        } else {
+          System.out.printf("\t%d", matrix[i][j]);
+        }
       }
 
     System.out.println();
