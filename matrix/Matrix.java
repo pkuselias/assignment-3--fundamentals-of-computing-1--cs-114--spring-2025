c5c83219aa64db2ea596a219e77391b3c1ae9c32
pkuselias <pkuselias@hartford.edu>
2025-04-07 14:44:03 -0400

    Create printMatrix method

--- matrix/Matrix.java
+++ matrix/Matrix.java
@@ -4,4 +4,16 @@ public class Matrix {
   public Matrix(int size) {
     matrix = new int[size][size];
   }
+
+  public void printMatrix() {
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[i].length; j++) {
+        System.out.printf("\t%d", matrix[i][j]);
+      }
+
+    System.out.println();
+    }
+
+  System.out.println();
+  }
 }
